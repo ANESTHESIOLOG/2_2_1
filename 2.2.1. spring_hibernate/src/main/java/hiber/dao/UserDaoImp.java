9214803f4b852f9b87e23d0d2c8697136f42a551
zotov <zotovegor952@gmail.com>
2022-12-27 23:02:45 +0300

    Решение

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,10 +1,13 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -17,6 +20,14 @@ public class UserDaoImp implements UserDao {
    @Override
    public void add(User user) {
       sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   public User selectUser(String model, int series) {
+      Query q = sessionFactory.getCurrentSession().createQuery("from User where userCar.model=:model and userCar.series=:series");
+         q.setParameter("model", model);
+         q.setParameter("series", series);
+      return (User) q.getSingleResult();
    }
 
    @Override
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,14 @@
 package hiber.service;
 
 import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
+
+    User selectUser(String model, int series);
+
     List<User> listUsers();
 }
